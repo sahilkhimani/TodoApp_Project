01b69dd1883a6a038edd01608c582ae6e61df1cc
Sahil Khimani <57582633+sahilkhimani@users.noreply.github.com>
2024-10-11 22:40:20 +0500

    Update api_url.dart

--- lib/services/api_url.dart
+++ lib/services/api_url.dart
@@ -1,4 +1,4 @@
 class ApiUrl {
   static var apiUrl = "https://api.todoist.com/rest/v2/tasks";
-  static var apiKey = "ee30b5759efde41eebf0b6126018b043bd016215";
+  static var apiKey = "wRiTeYoUrApIkEy";
 }
